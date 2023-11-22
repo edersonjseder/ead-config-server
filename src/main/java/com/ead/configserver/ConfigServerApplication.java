62cf27538e4fba3a8a1ee2fdd6ff5c5353a5a46e
Ederson de Jesus <edersonjseder@gmail.com>
2023-11-22 13:40:19 -0500

    Updated yml files and created database config

--- src/main/java/com/ead/configserver/ConfigServerApplication.java
+++ src/main/java/com/ead/configserver/ConfigServerApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableConfigServer
 @RequiredArgsConstructor
+@EnableTransactionManagement
 public class ConfigServerApplication implements CommandLineRunner {
 	private final UserUtils userUtils;
 	public static void main(String[] args) {
